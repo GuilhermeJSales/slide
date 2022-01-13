bd164a1ae20566d3bc308365da1a1fe76c725e3c
Guilherme Jesus Sales <guijsalesdg@outlook.com>
2022-01-12 23:01:07 -0300

    final da navegacao prev e nav

--- js/slide.js
+++ js/slide.js
@@ -6,6 +6,11 @@ export default class slide {
     this.dist = { finalPosition:0, startX:0, movement:0 }
   }
 
+
+  transition(active){
+    this.slide.style.transition = active ? 'transform .3s' : '';
+  }
+
   moveSlide(distX){
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;  
@@ -29,6 +34,7 @@ export default class slide {
     movetype = 'touchmove'
     }
     this.wrapper.addEventListener(movetype, this.onMove);
+    this.transiton(false);
   }
 
   onMove(event){    
@@ -41,6 +47,19 @@ export default class slide {
     const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(moveType , this.onMove);   
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true);
+    this.changeSlideOnEnd();    
+  }
+
+  changeSlideOnEnd(){
+    if(this.dist.movement > 120 && this.index.next !== undefined) { 
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined){
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active);
+    }
+
   }
 
   addSlideEvents(){
@@ -72,7 +91,7 @@ export default class slide {
 
   slideIndexNav(index){
     const last = this.slideArray.length -1;
-    return {
+    this.index = {
       prev: index ? index - 1 : undefined,
       active: index,
       next: index === last ? undefined : index + 1,
@@ -87,9 +106,20 @@ export default class slide {
   }
 
 
+
+  activePrevSlide(){
+    if(this.index.prev !== undefined ) this.changeSlide(this.index.prev);
+  }
+
+  activeNextSlide(){
+    if(this.index.next !== undefined ) this.changeSlide(this.index.next);
+  }
+
+
   init(){
     this.bindEvents();
     this.addSlideEvents();   
+    this.transition(true);
     this.slidesConfig();
     return this;
   }
